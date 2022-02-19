28f978b1a28aa2ff823a133b4356e35fc3a542a2
NikolasK88 <maxsuccess4@gmail.com>
2022-02-19 09:36:40 +0200

    first commit

---
+++ internal/app/apiserver/config.go
@@ -0,0 +1,24 @@
+package apiserver
+
+import(
+  "github.com/NikolasK88/golang-rest-api/internal/app/store"
+  
+)
+
+//Config
+type Config struct {
+  BindAddr string `toml:"bind_addr"`
+  LogLevel string `toml:"log_level"`
+  Store *store.Config
+
+}
+
+//NewConfig
+func NewConfig() *Config {
+  return &Config{
+    BindAddr:   ":8080",
+    LogLevel:   "debug",
+    Store:      store.NewConfig(),
+
+  }
+}
---
+++ internal/app/store/store.go
@@ -0,0 +1,41 @@
+package store
+
+import(
+  "database/sql"
+
+  _ "github.com/lib/pq" //...
+)
+
+//Store
+type Store struct {
+  config *Config
+  db *sql.DB
+
+}
+
+//New
+func New(config *Config) *Store {
+  return &Store {
+    config: config,
+  }
+}
+
+//Func for opening db
+func (s *Store) Open() error {
+  db, err := sql.Open("postgres", s.config.DatabaseURL)
+  if err != nil {
+    return err
+  }
+  if err := db.Ping(); err != nil {
+    return err
+  }
+
+  s.db = db
+
+  return nil
+}
+
+//Func for closing db
+func (s *Store) Close() {
+  s.db.Close()
+}
